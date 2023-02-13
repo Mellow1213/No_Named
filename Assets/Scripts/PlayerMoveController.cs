3de5b92a507caec6a078db7a33cd1c52a83cd8d7
JoInHo <zjxps2007@gmail.com>
2023-02-13 14:04:59 +0900

    Add Component

--- Assets/Scripts/PlayerMoveController.cs
+++ Assets/Scripts/PlayerMoveController.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class PlayerMoveController : MonoBehaviour
 {
+    [Header("Player")]
+    [Tooltip("Move speed of the character in m/s")]
+    public float MoveSpeed = 10.0f;
+
+    [Tooltip("Move speed of the character in m/s")]
+    public float SprintSpeed = 20.0f;
+
+    // Player
+    private CharacterController _controller;
+    private Animator _animator;
+
+    private bool _hasAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        //
+        _controller = GetComponent<CharacterController>();
+        _hasAnimator = TryGetComponent(out _animator);
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        _hasAnimator = TryGetComponent(out _animator);
+        
+        Move();
+    }
+
+    // Player Move
+    private void Move()
     {
         
     }
